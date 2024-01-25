c17c4e57227507fab40a534ddc91e40c84171b65
Foxglovery <foxglovery@gmail.com>
2024-01-25 14:32:06 -0600

    finished admin venue list and details

--- client/src/components/managers/venueManager.js
+++ client/src/components/managers/venueManager.js
@@ -3,3 +3,6 @@ const _apiUrl = "/api/Venue"
 export const GetVenues = () => {
     return fetch(_apiUrl).then((res) => res.json());
 }
+export const GetVenueById = (id) => {
+    return fetch(`${_apiUrl}/${id}`).then((res) => res.json());
+}
