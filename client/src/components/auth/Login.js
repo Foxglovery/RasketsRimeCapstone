8687e2af4a465db2b3f43dc105a41ab6181187c3
Foxglovery <foxglovery@gmail.com>
2024-02-03 12:25:09 -0600

    fixed auth pages

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { login } from "../managers/authManager";
 import { Button, FormFeedback, FormGroup, Input, Label } from "reactstrap";
-
+import styles from "../styles/auth/Login.module.css";
 export default function Login({ setLoggedInUser }) {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
@@ -22,40 +22,48 @@ export default function Login({ setLoggedInUser }) {
   };
 
   return (
-    <div className="container" style={{ maxWidth: "500px" }}>
-      <h3>Login</h3>
-      <FormGroup>
-        <Label>Email</Label>
-        <Input
-          invalid={failedLogin}
-          type="text"
-          value={email}
-          onChange={(e) => {
-            setFailedLogin(false);
-            setEmail(e.target.value);
-          }}
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label>Password</Label>
-        <Input
-          invalid={failedLogin}
-          type="password"
-          value={password}
-          onChange={(e) => {
-            setFailedLogin(false);
-            setPassword(e.target.value);
-          }}
-        />
-        <FormFeedback>Login failed.</FormFeedback>
-      </FormGroup>
-
-      <Button color="primary" onClick={handleSubmit}>
-        Login
-      </Button>
-      <p>
-        Not signed up? Register <Link to="/register">here</Link>
-      </p>
+    <div className={styles["login-background-image"]}>
+      <div className={styles["login-card"]}>
+        <div className={styles["container"]}>
+          <h3>Login</h3>
+          <FormGroup>
+            <Label>Email</Label>
+            <Input
+              invalid={failedLogin}
+              type="text"
+              value={email}
+              onChange={(e) => {
+                setFailedLogin(false);
+                setEmail(e.target.value);
+              }}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Password</Label>
+            <Input
+              invalid={failedLogin}
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setFailedLogin(false);
+                setPassword(e.target.value);
+              }}
+            />
+            <FormFeedback>Login failed.</FormFeedback>
+          </FormGroup>
+          <div className={styles["login-btn-ctn"]}>
+            <Button className={styles["login-btn"]} onClick={handleSubmit}>
+              Come On In!
+            </Button>
+          </div>
+              <div className={styles["login-reg-ctn"]}>
+                <p>
+            Not signed up? Register <Link to="/register">here</Link>
+          </p>
+              </div>
+          
+        </div>
+      </div>
     </div>
   );
 }
